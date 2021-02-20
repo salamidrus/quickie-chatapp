a5dd4ccf066c7f0633de95ca3343a83279be29d0
salamidrus <idrussalam95gmail.com>
2021-02-20 22:37:52 +0700

    Add Validator for login

--- server/validation/login.js
+++ server/validation/login.js
@@ -5,26 +5,12 @@ module.exports = validateRegisterInput = (data) => {
   let errors = {};
 
   // Convert empty fields to String in order to validate them
-  let fields = ["name", "username", "password", "confirmPassword"];
+  let fields = ["username", "password"];
   for (let key of fields) {
     data[key] = !isEmpty(data[key]) ? data[key] : "";
 
     if (Validator.isEmpty(data[key])) {
       errors[key] = `${key} field is required`;
-    }
-
-    if (
-      key == "password" &&
-      !Validator.isLength(data.password, { min: 6, max: 30 })
-    ) {
-      errors[key] = `${key} must be at least 6 characters`;
-
-      if (
-        key == "confirmPassword" &&
-        !Validator.equals(data.password, data.password2)
-      ) {
-        errors.confirmPassword = "Passowrds must match";
-      }
     }
   }
 
