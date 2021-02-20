c2143256a04d2958f3264274d0f7f88fcb77794d
salamidrus <idrussalam95gmail.com>
2021-02-20 21:21:50 +0700

    Add Passport and Socket

--- server/index.js
+++ server/index.js
@@ -1,18 +1,44 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
-const path = require("path");
-const io = require("socket.io")(app);
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cors = require("cors");
 
-const uri = process.env.MONGODB_URI;
+const app = express();
+
 const port = process.env.PORT || 5000;
 
-const Message = require("./model/Message");
-const mongoose = require("mongoose");
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port}`)
+);
 
-mongoose.connect(uri, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+const io = require("socket.io").listen(server);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// CORS
+app.use(cors());
+
+// Database Configuration
+const db = process.env.MONGODB_URI;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Successfully Connected!"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport function
+let passportMiddleware = require("./middleware/passport");
+passportMiddleware(passport);
+
+// ASsign socket object to every request
+app.use((req, res, next) => {
+  req.io = io;
+  next();
 });
-
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
