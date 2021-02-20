295c38c3707795d8f3bfef6561b2658e9d31218a
salamidrus <idrussalam95gmail.com>
2021-02-20 20:29:44 +0700

    Add Mongodb Setup

--- server/index.js
+++ server/index.js
@@ -6,3 +6,13 @@ const io = require("socket.io")(app);
 
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
+
+const Message = require("./model/Message");
+const mongoose = require("mongoose");
+
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
