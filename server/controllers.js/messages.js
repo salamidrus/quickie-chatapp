1bb1b24d2d9a9cf1ca10923ac962f2ee2282b944
salamidrus <idrussalam95gmail.com>
2021-02-22 21:22:33 +0700

    Add Get Conversations Controller

--- server/controllers.js/messages.js
+++ server/controllers.js/messages.js
@@ -2,6 +2,9 @@ const Message = require("../models/Message");
 const Conversation = require("../models/Conversation");
 const GlobalMessage = require("../models/GlobalMessage");
 
+// library
+const mongoose = require("mongoose");
+
 exports.GetAllGlobalMessages = async (req, res) => {
   try {
     let data = await GlobalMessage.find().populate("from", "username name");
@@ -31,6 +34,28 @@ exports.CreateGlobalMessage = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Successfully create global message!",
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failure to Get the Data!",
+    });
+  }
+};
+
+exports.GetConversations = async (req, res) => {
+  try {
+    let from = mongoose.Types.ObjectId(req.user.userId);
+    let data = await Conversation.find({
+      recipients: {
+        $all: [{ $elemMatch: { $eq: from } }],
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully get the conversations!",
       data,
     });
   } catch (err) {
