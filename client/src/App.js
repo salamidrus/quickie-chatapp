ff069ddc72d06fceec5126753818f9d51e590530
salamidrus <idrussalam95gmail.com>
2021-02-23 21:52:28 +0700

    Setup MaterialUI for Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import { SnackbarProvider } from "notistack";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#58a5f0",
+      main: "#0277bd",
+      dark: "#004c8c",
+    },
+    secondary: {
+      light: "#ffd95a",
+      main: "#f9a825",
+      dark: "#c17900",
+      contrastText: "#212121",
+    },
+    background: {
+      default: "#f0f0f0",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SnackbarProvider maxSnack={3} autoHideDuration={3000}></SnackbarProvider>
+    </ThemeProvider>
   );
 }
 
