6cf0e2761bce9011e055d9e6cfd65bf6c3493f46
salamidrus <idrussalam95gmail.com>
2021-03-07 21:30:33 +0700

    Add Path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
