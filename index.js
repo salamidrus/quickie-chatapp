0598562b179e4be36cefaa72003611d2e6f058ca
salamidrus <idrussalam95gmail.com>
2021-03-07 19:31:12 +0700

    Add Static Serve on Production Build

--- index.js
+++ index.js
@@ -50,6 +50,16 @@ app.use((req, res, next) => {
   next();
 });
 
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Routes
 let userRoutes = require("./routes/users");
 let messageRoutes = require("./routes/messages");
